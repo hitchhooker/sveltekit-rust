64cdc89ac0f1cfe3b4b29ce8a535562530868dda
hitchhooker <tommi@niemi.lol>
2022-10-01 22:10:56 +0100

    moi

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import rust from '@wasm-tool/rollup-plugin-rust';
 
+
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
 		sveltekit(),
-		rust({
-			verbose: true,
-			serverPath: "",
-			inlineWasm: true,
-			
-		})
+		{ // https://vitejs.dev/guide/api-plugin.html#rollup-plugin-compatibility adviced here
+			...rust({
+				verbose: true,
+				serverPath: "",
+				inlineWasm: true,
+			}),
+			enforce: 'post',
+			apply: 'build'
+		}
 	]
 };
 
