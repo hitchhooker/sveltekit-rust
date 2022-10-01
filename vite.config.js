91a3e3f55af849dc77d6aa82077296e8aafdf35d
hitchhooker <tommi@niemi.lol>
2022-10-01 22:15:21 +0100

    add inspect

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import rust from '@wasm-tool/rollup-plugin-rust';
+import Inspect from 'vite-plugin-inspect'
 
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
+	    Inspect(),
 		sveltekit(),
 		{ // https://vitejs.dev/guide/api-plugin.html#rollup-plugin-compatibility adviced here
 			...rust({
